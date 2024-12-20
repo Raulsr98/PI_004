21f5573e8daa394d861843b5e4892fcbe1688007
Raulsr98 <rasaro01@alumnatflorida.es>
2024-12-20 21:40:31 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
-console.log("hola mundo");
+const express = require("express");
+const app = express();
+
+app.set("port", process.env.PORT || 8080);
+
+app.listen(app.get("port"), leerPuerto());
+
+function leerPuerto() {
+  console.log("Hola desde el puerto " + app.get("port"));
+}
